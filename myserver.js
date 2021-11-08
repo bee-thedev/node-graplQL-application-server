d9eb66a7d6234b922ac3df48947861deb3b94bc5
Beenysh Sara <beenyshsaeed@gmail.com>
2021-11-08 21:33:50 +0500

    ~Express server added~

---
+++ myserver.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const users =[
+    {
+        id: 0,
+        name: "Beenysh",
+        email: "beenysh@gmail.com"
+    },
+    {
+        id: 1,
+        name: "Mehwysh",
+        email: "mehwysh@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Omar",
+        email: "omar@gmail.com"
+    }
+]
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get(/.*fly$/, function (req, res) {
+  res.send('/.*fly$/')
+})
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.all('/secret', function (req, res, next) {
+  console.log('Accessing the secret section ...')
+  next() // pass control to the next handler
+})
+
+app.get('/users', (req, res) => {
+    res.json(users)
+  })
+
+app.get('/blog', (req, res) => {
+    res.send('<h1>Stumble Upon Words</h1>')
+  })
+
+  app.get('/queryString', (req, res) => {
+    console.log(req.query)
+    res.send(req.query)
+  })
+  
+  app.get('/users/:id', (req, res) => {
+    console.log(req.params)
+    const {id} = req.params
+    res.json(users[id])
+  })
+
+//   app.get('/blog/:id', (req, res) => {
+//     console.log(req.params)
+//     res.json(Number(req.params.id))
+//   })
+
+  app.post('/blog', (req, res) => {
+    res.send('<h1>Stumble Upon Words</h1>')
+    console.log(res)
+  })
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
